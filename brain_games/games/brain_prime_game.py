76d5284c022e88d52b2e48509c10d996b985c7f1
Elizaveta <ukrop@LAPTOP-T43LJSG0>
2023-07-12 19:14:38 +0300

    modify brain_prime_game.py

--- brain_games/games/brain_prime_game.py
+++ brain_games/games/brain_prime_game.py
@@ -13,33 +13,36 @@ def give_question():
     return random_number
 
 
+def right_answer(number):
+    result = 0
+    for divider in range(2, int(number/2)):
+        if number % divider == 0:
+            result = 1
+            break
+        else:
+            continue
+
+    if result == 0:
+        right_answer = 'yes'
+    else:
+        right_answer = 'no'
+    return right_answer
+
+
 def check_answer(users_name):
     i = 1
     while i <= number_of_tries:
-        question = give_question()
-        number = question
+        number = give_question()
         answer = accept_users_answer()
+        correct_answer = right_answer(number)
 
-        result = 0
-        for divider in range(2, int(number/2)):
-            if number % divider == 0:
-                result = 1
-                break
-            else:
-                continue
-
-        if result == 0:
-            right_answer = 'yes'
-        else:
-            right_answer = 'no'
-
-        if right_answer == answer:
+        if correct_answer == answer:
             print('Correct!')
             i = i + 1
             continue
         else:
             print(f'''
-            {answer} is wrong answer ;(. Correct answer was {right_answer}
+            {answer} is wrong answer ;(. Correct answer was {correct_answer}
             Let\'s try again, {users_name}!
             ''')
             i = 1
