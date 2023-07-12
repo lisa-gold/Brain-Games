fff2cc37e99fd80155117b21850370e4c1dda012
Elizaveta <ukrop@LAPTOP-T43LJSG0>
2023-07-12 14:03:08 +0300

    add brain_progression.py and brain_progression_game.py

---
+++ brain_games/games/brain_progression_game.py
@@ -0,0 +1,53 @@
+from random import randint
+from brain_games.all_games import number_of_tries
+from brain_games.all_games import accept_users_answer
+
+
+def introduce_rules():
+    print('What number is missing in the progression?')
+
+
+def give_question():
+    random_number_1 = randint(0, 99)
+    random_progressor = randint(0, 9)
+    random_lenth = randint(5, 15)
+    random_operation_index = randint(1, 2)
+    result = [random_number_1]
+    if random_operation_index == 1:
+        for i in range(1, random_lenth):
+            new_value = random_number_1 + random_progressor * i
+            result.append(new_value)
+    else:
+        for i in range(1, random_lenth):
+            new_value = random_number_1 - random_progressor * i
+            result.append(new_value)
+    random_place = randint(0, random_lenth - 1)
+    result_clone = result[:]
+    result_clone[random_place] = '..'
+    result_to_show = ''
+    for i in range(len(result_clone)):
+        result_to_show = result_to_show + str(result_clone[i]) + ' '
+    print(f'Question: {result_to_show}')
+    return result, random_place
+
+
+def check_answer(users_name):
+    i = 1
+    while i <= number_of_tries:
+        question = give_question()
+        answer = int(accept_users_answer())
+        random_place = question[1]
+        right_answer = question[0][random_place]
+        if right_answer == answer:
+            print('Correct!')
+            i = i + 1
+            continue
+        else:
+            print(f'''
+            {answer} is wrong answer ;(. Correct answer was {right_answer}
+            Let\'s try again, {users_name}!
+            ''')
+            i = 1
+            continue
+
+    return print(f'Congratulations, {users_name}!')
---
+++ brain_games/scripts/brain_progression.py
@@ -0,0 +1,15 @@
+from brain_games.all_games import greet
+from brain_games.cli import welcome_user
+from brain_games.games.brain_progression_game import introduce_rules
+from brain_games.games.brain_progression_game import check_answer
+
+
+def main():
+    greet()
+    name = welcome_user()
+    introduce_rules()
+    check_answer(name)
+
+
+if __name__ == '__main__':
+    main()
