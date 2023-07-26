9250a35192fb3b96e8d60ada07204c5b78ccde74
Elizaveta <ukrop@LAPTOP-T43LJSG0>
2023-07-26 11:34:11 +0300

    corrections in calc.py

--- brain_games/games/calc.py
+++ brain_games/games/calc.py
@@ -8,16 +8,12 @@ def give_question():
     random_number_1 = randint(0, 99)
     random_number_2 = randint(0, 99)
     random_operator = choice('+-*')
-    right_answer = random_number_1
     match random_operator:
-        case 1:
-            random_operator = '+'
-            right_answer = right_answer + random_number_2
-        case 2:
-            random_operator = '-'
-            right_answer = right_answer - random_number_2
-        case 3:
-            random_operator = '*'
-            right_answer = right_answer * random_number_2
+        case '+':
+            right_answer = random_number_1 + random_number_2
+        case '-':
+            right_answer = random_number_1 - random_number_2
+        case '*':
+            right_answer = random_number_1 * random_number_2
     expression = f'{random_number_1} {random_operator} {random_number_2}'
     return expression, str(right_answer)
